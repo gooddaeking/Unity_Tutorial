43dc6c093e2223a793845862620fc9ed1940e1be
gooddaeking <gooddaeking@gmail.com>
2020-05-24 04:18:19 +0900

    6. Camera

---
+++ Unity_Tutorial/Assets/Scripts/Scene6.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene6 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject go_Target;
+
+    [SerializeField]
+    private float speed;
+
+    // 카메라 속성을 바꿀 수 있다.
+    private Camera theCam;
+
+    private Vector3 difValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        difValue = this.transform.position - go_Target.transform.position;
+        difValue = new Vector3(Mathf.Abs(difValue.x), Mathf.Abs(difValue.y), Mathf.Abs(difValue.z));
+
+        //theCam.fieldOfView = 시야
+        //theCam.clearFlags = 백그라운드설정
+        //theCam.nearClipPlane =
+        //theCam.farClipPlane =
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, go_Target.transform.position + difValue, speed);
+    }
+}
