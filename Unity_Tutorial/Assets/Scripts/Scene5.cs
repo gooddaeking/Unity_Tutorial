a11d9da203042dad7b686f79fe65746c90a522e2
gooddaeking <gooddaeking@gmail.com>
2020-05-24 03:08:01 +0900

    5. MeshRenderer

---
+++ Unity_Tutorial/Assets/Scripts/Scene5.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene5 : MonoBehaviour
+{
+    [SerializeField]
+    private Material yellow_Mat;
+    [SerializeField]
+    private Material purple_Mat;
+
+    private MeshRenderer mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetMouseButton(0))
+        {
+            mesh.material = yellow_Mat;
+        }
+        else
+        {
+            mesh.material = purple_Mat;
+        }
+
+    }
+}
