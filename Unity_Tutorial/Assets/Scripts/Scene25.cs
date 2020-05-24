e3af4acd125423a55adaf8048a3a88bfd69ee30e
gooddaeking <gooddaeking@gmail.com>
2020-05-25 05:30:32 +0900

    25. Mobile Pad

---
+++ Unity_Tutorial/Assets/Scripts/Scene25.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Scene25 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler 
+{
+    [SerializeField] private RectTransform rect_Background;
+    [SerializeField] private RectTransform rect_Joystick;
+
+    private float radius;
+
+    [SerializeField] private GameObject go_Player;
+    [SerializeField] private float moveSpeed;
+
+    private bool isTouch = false;
+    private Vector3 movePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = rect_Background.rect.width * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTouch)
+        {
+            go_Player.transform.position += movePosition;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Vector2 value = eventData.position - (Vector2)rect_Background.position;
+
+        value = Vector2.ClampMagnitude(value, radius);
+        rect_Joystick.localPosition = value;
+
+        float distance = Vector2.Distance(rect_Background.position, rect_Joystick.position) / radius;
+        value = value.normalized;
+        movePosition = new Vector3(value.x * moveSpeed * Time.deltaTime * distance, 0f, value.y * moveSpeed * Time.deltaTime * distance);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        isTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+        //throw new System.NotImplementedException();
+        isTouch = false;
+        rect_Joystick.localPosition = Vector3.zero;
+        movePosition = Vector3.zero;
+    }
+}
