a7fe1377528731082911778b931afd02e5d3175a
gooddaeking <gooddaeking@gmail.com>
2020-05-25 04:37:51 +0900

    23. Drag & Drop

---
+++ Unity_Tutorial/Assets/Scripts/Scene23.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Scene23 : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        //Debug.Log("Drag");
+        transform.position = eventData.position;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+        Debug.Log("Drop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("End Drag");
+    }
+}
