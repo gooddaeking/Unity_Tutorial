4312bd10b66d3d75e3b7fc94a6a90a5b19a74b8e
gooddaeking <gooddaeking@gmail.com>
2020-05-29 17:00:30 +0900

    뉴프로젝트 드레스업 추가

---
+++ Avatar/Assets/_HJW/Scripts/ButtonTest.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    Button next;
+    Button prev;
+
+    public GameObject[] hair;
+    public GameObject[] upBody;
+    public GameObject[] downBody;
+
+    int[] save = { 0, 0, 0 };
+    int[] load = { 0, 0, 0 };
+
+    int currHair = 0;
+    int currUpbody;
+    int currDownbody;
+
+    private void Update()
+    {
+        for (int i = 0; i < hair.Length; i++)
+        {
+            if (i == currHair)
+                hair[i].SetActive(true);
+            else
+                hair[i].SetActive(false);
+        }
+        for (int i = 0; i < upBody.Length; i++)
+        {
+            if (i == currUpbody)
+                upBody[i].SetActive(true);
+            else
+                upBody[i].SetActive(false);
+        }
+        for (int i = 0; i < hair.Length; i++)
+        {
+            if (i == currDownbody)
+                downBody[i].SetActive(true);
+            else
+                downBody[i].SetActive(false);
+        }
+
+
+    }
+
+    public void NextHair()
+    {
+        currHair++;
+        Debug.Log(currHair);
+        if (currHair > hair.Length - 1)
+            currHair = 0;
+    }
+
+    public void PrevHair()
+    {
+        currHair--;
+        Debug.Log(currHair);
+        if (currHair < 0)
+            currHair = hair.Length - 1;
+    }
+    public void NextUp()
+    {
+        currUpbody++;
+        Debug.Log(currUpbody);
+        if (currUpbody > upBody.Length - 1)
+            currUpbody = 0;
+    }
+
+    public void PrevUp()
+    {
+        currUpbody--;
+        Debug.Log(currUpbody);
+        if (currUpbody < 0)
+            currUpbody = upBody.Length - 1;
+    }
+    public void NextDown()
+    {
+        currDownbody++;
+        Debug.Log(currDownbody);
+        if (currDownbody > downBody.Length - 1)
+            currDownbody = 0;
+    }
+
+    public void PrevDown()
+    {
+        currDownbody--;
+        Debug.Log(currDownbody);
+        if (currDownbody < 0)
+            currDownbody = downBody.Length - 1;
+    }
+
+    public void Save()
+    {
+        save[0] = currHair;
+        save[1] = currUpbody;
+        save[2] = currDownbody;
+    }
+
+    public void Load()
+    {
+        currHair = save[0];
+        currUpbody = save[1];
+        currDownbody = save[2];
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RayTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEditor;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour
+{
+    public float rayDistance = 2.0f;
+    Ray ray;            //레이 구조체
+    RaycastHit hitInfo;  //레이캐스트 힛(레이랑 충돌된 오브젝트의 정보를 담아두는 구조체)
+    //RaycastHit[] rayHit;  //레이캐스트 힛(레이랑 충돌된 오브젝트의 정보를 담아두는 구조체)
+
+    void Start()
+    {
+        //레이 구조체 생성 후 값 초기화
+        ray = new Ray();
+        ray.origin = transform.position;
+        ray.direction = transform.forward;
+
+        //레이캐스트힛 구조체 초기화
+        hitInfo = new RaycastHit();
+        //rayHit = Physics.RaycastAll(ray, rayDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+        if (Physics.Raycast(transform.position, transform.forward - transform.right, rayDistance))
+        {
+            transform.eulerAngles += new Vector3(0, 15, 0);
+            print("발사한 오브젝트가 충돌됨");
+        }
+        if(Physics.Raycast(transform.position, transform.forward + transform.right, rayDistance))
+        {
+            transform.eulerAngles += new Vector3(0, -15, 0);
+            print("발사한 오브젝트가 충돌됨");
+        }
+        Debug.DrawRay(transform.position, (transform.forward - transform.right) * rayDistance, Color.green, 2.0f);
+        Debug.DrawRay(transform.position, (transform.forward + transform.right) * rayDistance, Color.green, 2.0f);
+        
+        //기본 레이캐스트
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("레이 발싸!!!!!!!!!!");
+            
+
+            //Raycast는 물리컴포넌트의 함수이기 때문에 Physics.Raycast로 함수를 호출해야 한다
+            if (Physics.Raycast(ray.origin, ray.direction, rayDistance))
+            {
+                print("발사한 오브젝트가 충돌됨");
+            }
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo,rayDistance))
+            {
+                print(hitInfo.collider.name);
+            }
+        }
+    }
+}
